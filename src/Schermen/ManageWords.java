303fff6f31a61d9a1199042b2b3d9df2e8d4ed4c
Sander de Haas <s.de.haas@outlook.com>
2013-12-04 12:11:57 +0100

    little outlook changes

--- src/Schermen/ManageWords.java
+++ src/Schermen/ManageWords.java
@@ -13,6 +13,7 @@ import logics.LoginLogics;
 import FormElements.MyButton;
 import FormElements.TextFieldSL;
 import Main.DatabaseController;
+import Main.FontController;
 import Main.Gui;
 
 
@@ -30,7 +31,13 @@ public class ManageWords extends JPanel {
 				AvansLabel.setBounds(20,gui.getHeight() - 21 - AvansLogo.getIconHeight(), AvansLogo.getIconWidth(), AvansLogo.getIconHeight());
 				this.add(AvansLabel);
 				
-				
+				//welkom tekst
+				JLabel welkomText = new JLabel("Manage words");
+				welkomText.setForeground(Color.white);
+				welkomText.setBounds(500,120,350,55);
+				welkomText.setFont(FontController.getInstance().getFont("segoeuil"));
+				welkomText.setFont(new Font(welkomText.getFont().getName(), Font.PLAIN, 42));
+				this.add(welkomText);	
 		
 	}
 	
